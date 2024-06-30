f6498d38dc3ecf1a9af3bf65b14aefe2936d08ed
basudev2016 <basudevpanda@gmail.com>
2024-06-30 15:53:56 +0530

    utils are added

---
+++ demo.py
@@ -0,0 +1,10 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+
+# logging.info ("Welcome to our custom Logger")
+
+# try:
+#     a = 1/ "aa"
+# except Exception as e:
+#     raise USvisaException(e,sys) from e
